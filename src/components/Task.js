281a5e53e2488242d5ac60811491c38dae9c706c
matsu <50922910+mtsml@users.noreply.github.com>
2023-08-19 03:49:49 +0900

    文字以外をタップしても遷移する #2

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,23 +1,26 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { MDBCheckbox } from 'mdb-react-ui-kit';
 
 
 const Task = ({ task, handleCheck }) => {
+    const navigate = useNavigate();
+
     return (
-        <div className="d-flex p-2 border-top border-bottom border-secondary">
+        <div
+            className="d-flex p-2 border-top border-bottom border-secondary"
+            onClick={() => navigate("/detail", { state: task })}
+        >
             <MDBCheckbox
                 inline
                 checked={task.done}
                 onChange={e => handleCheck(task.id, e.target.checked)}
             />
-            <Link
+            <span
                 style={task.done ? { textDecoration: "line-through" } : {}}
-                to="/detail"
-                state={task}
             >
                 {task.value}
-            </Link>
+            </span>
         </div>
     )
 }
