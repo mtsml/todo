f63aef70eb9dd76e625c3a1ccdb3c4f130f38911
matsu <50922910+mtsml@users.noreply.github.com>
2023-08-26 22:49:55 +0900

    リストを1行に並べる#11

--- src/components/Header.js
+++ src/components/Header.js
@@ -19,19 +19,21 @@ export const Header = () => {
   };
 
   return (
-    <header className='d-flex justify-content-between overflow-x-scroll'>
-        <MDBTabs className='mb-3 text-nowrap' style={{width: "max-content"}}>
-            {list && list.map(l => (
-                <MDBTabsItem key={l.id}>
-                    <MDBTabsLink
-                        onClick={() => !l.isActive && handleFillClick(l.id)}
-                        active={l.isActive}
-                    >
-                        {l.name}
-                    </MDBTabsLink>
-                </MDBTabsItem>
-            ))}
-        </MDBTabs>
+    <header className='d-flex justify-content-between'>
+        <div className="overflow-x-scroll">
+            <MDBTabs className='mb-3 text-nowrap ' style={{width: "max-content"}}>
+                {list && list.map(l => (
+                    <MDBTabsItem key={l.id}>
+                        <MDBTabsLink
+                            onClick={() => !l.isActive && handleFillClick(l.id)}
+                            active={l.isActive}
+                        >
+                            {l.name}
+                        </MDBTabsLink>
+                    </MDBTabsItem>
+                ))}
+            </MDBTabs>
+        </div>
         <MDBIcon
             className="pe-2 pt-2"
             style={{zIndex: 10}}
